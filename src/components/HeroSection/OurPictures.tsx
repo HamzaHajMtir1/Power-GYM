aba25baae66103139bf7bd0303d1719d72a66163
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-09-15 00:30:58 +0100

    Start building the home page

---
+++ src/components/HeroSection/OurPictures.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+const OurPictures: React.FC = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <div className="mt-32 overflow-hidden sm:mt-40">
+      {isClient && (
+        <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
+            <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
+              <p className="mt-6 text-xl leading-8 text-gray-600">
+                Power GYM description
+              </p>
+            </div>
+            <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+              <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+                <Image
+                  fetchPriority="high"
+                  quality={100}
+                  priority={true}
+                  layout="aspect-fill"
+                  height={500}
+                  width={700}
+                  src="/HomePage/Pict1.webp"
+                  alt="Power GYM Space."
+                  className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
+              <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
+                <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+                  <Image
+                    fetchPriority="high"
+                    quality={100}
+                    priority={true}
+                    layout="aspect-fill"
+                    height={500}
+                    width={700}
+                    src="/HomePage/Pict2.webp"
+                    alt="Power GYM Space."
+                    className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                  />
+                </div>
+                <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+                  <Image
+                    fetchPriority="high"
+                    quality={100}
+                    priority={true}
+                    layout="aspect-fill"
+                    height={500}
+                    width={700}
+                    src="/HomePage/Pict3.webp"
+                    alt="Power GYM Space."
+                    className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                  />
+                </div>
+                <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+                  <Image
+                    fetchPriority="high"
+                    quality={100}
+                    priority={true}
+                    layout="aspect-fill"
+                    height={500}
+                    width={700}
+                    src="/HomePage/Pict4.webp"
+                    alt="Power GYM Space."
+                    className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OurPictures;
---
+++ src/components/HeroSection/OurServices.tsx
@@ -0,0 +1,175 @@
+import React from 'react';
+
+export const OurServices: React.FC = () => {
+  return (
+    <div className="bg-white px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase sm:text-center">
+        20 Nov 2020
+      </p>
+      <div className="max-w-xl mb-5 md:mx-auto sm:text-center lg:max-w-2xl">
+        <div className="mb-4">
+          <a
+            href="/"
+            aria-label="Article"
+            className="inline-block max-w-lg font-sans text-3xl font-extrabold leading-none tracking-tight text-black transition-colors duration-200 hover:text-deep-purple-accent-700 sm:text-4xl"
+          >
+            The quick, brown fox jumps over a lazy dog
+          </a>
+        </div>
+        <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p>
+      </div>
+      <div className="mb-10 sm:text-center">
+        <a href="/" aria-label="Author" className="inline-block mb-1">
+          <img
+            alt="avatar"
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            className="object-cover w-10 h-10 rounded-full shadow-sm"
+          />
+        </a>
+        <div>
+          <a
+            href="/"
+            aria-label="Author"
+            className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
+          >
+            Vasile Melinte
+          </a>
+          <p className="text-sm font-medium leading-4 text-gray-600">Author</p>
+        </div>
+      </div>
+      <div className="sm:text-center">
+        <a
+          href="/"
+          aria-label=""
+          className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+        >
+          Read more
+        </a>
+      </div>
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+            <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+            <img
+                src="https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+                className="object-cover w-full h-64"
+                alt=""
+            />
+            <div className="p-5 border border-t-0">
+                <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+                <a
+                    href="/"
+                    className="transition-colors duration-200 text-black hover:text-deep-purple-accent-700"
+                    aria-label="Category"
+                    title="traveling"
+                >
+                    traveling
+                </a>
+                <span className="text-gray-600">— 28 Dec 2020</span>
+                </p>
+                <a
+                href="/"
+                aria-label="Category"
+                title="Visit the East"
+                className="inline-block mb-3 text-black text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+                >
+                Visit the East
+                </a>
+                <p className="mb-2 text-gray-700">
+                Sed ut perspiciatis unde omnis iste natus error sit sed quia
+                consequuntur magni voluptatem doloremque.
+                </p>
+                <a
+                href="/"
+                aria-label=""
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                >
+                Learn more
+                </a>
+            </div>
+            </div>
+            <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+            <img
+                src="https://images.pexels.com/photos/447592/pexels-photo-447592.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                className="object-cover w-full h-64"
+                alt=""
+            />
+            <div className="p-5 border border-t-0">
+                <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+                <a
+                    href="/"
+                    className="transition-colors duration-200 text-black hover:text-deep-purple-accent-700"
+                    aria-label="Category"
+                    title="traveling"
+                >
+                    traveling
+                </a>
+                <span className="text-gray-600">— 28 Dec 2020</span>
+                </p>
+                <a
+                href="/"
+                aria-label="Category"
+                title="Simple is better"
+                className="inline-block mb-3 text-black text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+                >
+                Simple is better
+                </a>
+                <p className="mb-2 text-gray-700">
+                Sed ut perspiciatis unde omnis iste natus error sit sed quia
+                consequuntur magni voluptatem doloremque.
+                </p>
+                <a
+                href="/"
+                aria-label=""
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                >
+                Learn more
+                </a>
+            </div>
+            </div>
+            <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+            <img
+                src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                className="object-cover w-full h-64"
+                alt=""
+            />
+            <div className="p-5 border border-t-0">
+                <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+                <a
+                    href="/"
+                    className="transition-colors duration-200 text-black hover:text-deep-purple-accent-700"
+                    aria-label="Category"
+                    title="traveling"
+                >
+                    traveling
+                </a>
+                <span className="text-gray-600">— 28 Dec 2020</span>
+                </p>
+                <a
+                href="/"
+                aria-label="Category"
+                title="Film It!"
+                className="inline-block mb-3 text-black text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+                >
+                Film It!
+                </a>
+                <p className="mb-2 text-gray-700">
+                Sed ut perspiciatis unde omnis iste natus error sit sed quia
+                consequuntur magni voluptatem doloremque.
+                </p>
+                <a
+                href="/"
+                aria-label=""
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                >
+                Learn more
+                </a>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  );
+};
