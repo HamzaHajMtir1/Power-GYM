2b1214b24b00dc62a62e24b717f469b0286e04b0
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-09-20 20:27:37 +0100

    Finished Our Pictures part

--- src/components/HeroSection/OurPictures.tsx
+++ src/components/HeroSection/OurPictures.tsx
@@ -16,12 +16,22 @@ const OurPictures: React.FC = () => {
         <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
             <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
-              <p className="mt-6 text-xl leading-8 text-gray-600">
-                Power GYM description
-              </p>
+              <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+                  <Image
+                    fetchPriority="high"
+                    quality={100}
+                    priority={true}
+                    layout="aspect-fill"
+                    height={500}
+                    width={700}
+                    src="/HomePage/Pict3.webp"
+                    alt="Power GYM Space."
+                    className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                  />
+                </div>
             </div>
             <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
-              <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+              <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
                 <Image
                   fetchPriority="high"
                   quality={100}
@@ -35,7 +45,7 @@ const OurPictures: React.FC = () => {
                 />
               </div>
               <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
-                <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+                <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
                   <Image
                     fetchPriority="high"
                     quality={100}
@@ -48,7 +58,7 @@ const OurPictures: React.FC = () => {
                     className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
                   />
                 </div>
-                <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+                <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
                   <Image
                     fetchPriority="high"
                     quality={100}
@@ -61,7 +71,7 @@ const OurPictures: React.FC = () => {
                     className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
                   />
                 </div>
-                <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+                <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
                   <Image
                     fetchPriority="high"
                     quality={100}
--- src/components/HeroSection/VideoSection.tsx
+++ src/components/HeroSection/VideoSection.tsx
@@ -17,6 +17,11 @@ const VideoSection: React.FC = () => {
     <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
       {isClient && (
         <>
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
+              know more about our gym
+            </h2>
+          </div>
           <div className="mx-auto mt-16 sm:mt-20 lg:mx-0 lg:max-w-none" ref={ref}>
             {inView && (
               <article className="relative isolate justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
