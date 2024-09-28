3dd5e4d6e27a2b8cc7cde64675b0ff53b6f86361
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-09-28 23:04:19 +0100

    Change the about section

--- src/components/AboutUs/AboutUsHeroSection.tsx
+++ src/components/AboutUs/AboutUsHeroSection.tsx
@@ -2,12 +2,16 @@
 
 export default function AboutUsHeroSection() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-      <img
-        alt=""
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      />
+    <div className="relative h-screen flex items-center justify-center overflow-hidden bg-black">
+      {/* Background image with overlay */}
+      <div className="absolute inset-0 z-0">
+        <img
+          src="/About/about.png"
+          alt="Gym background"
+          className="w-full h-full object-cover opacity-50"
+        />
+        <div className="absolute inset-0" />
+      </div>
       <div
         aria-hidden="true"
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
@@ -34,10 +38,7 @@ export default function AboutUsHeroSection() {
       </div>
       <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">About Us</h2>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-          This page provides key information about our gym.
-          </p>
+          
         </div>
        
       </div>
